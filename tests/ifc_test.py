7d26c3b24d17befe6304a6b8301a76aaee27b349
AhmedHany1212 <117445830+AhmedHany1212@users.noreply.github.com>
2023-02-04 17:23:44 +0200

    Update ifc_test.py

--- tests/ifc_test.py
+++ tests/ifc_test.py
@@ -36,13 +36,17 @@ def cover(a, b):
 
 @cocotb.test()
 async def ifc_test(dut):
+    dut.ifc_test.a_ff.(CLK,D_IN,ENQ,DEQ,CLR,D_OUT,FULL_N,EMPTY_N)
+    dut.ifc_test.b_ff.(CLK,D_IN,ENQ,DEQ,CLR,D_OUT,FULL_N,EMPTY_N)
+    dut.ifc_test.y_ff.(CLK,D_IN,ENQ,DEQ,CLR,D_OUT,FULL_N,EMPTY_N)
+
     dut.RST_N.value = 1
     await Timer(1, 'ns')
     dut.RST_N.value = 0
     await Timer(1, 'ns')
     await RisingEdge(dut.CLK)
     dut.RST_N.value = 1
-    writedrv = InputDriver(dut, 'write', dut.CLK)
+    writedrv = InpumtDriver(dut, 'write', dut.CLK)
     InputMonitor(dut, 'write', dut.CLK, callback=a_cover)
     readdrv=OutputDriver(dut, 'read', dut.CLK, sb_fn)
     
@@ -166,8 +170,19 @@ class OutputDriver(BusDriver):
         self.bus.address.value = value  
         await ReadOnly()
         self.callback(self.bus.data.value)
-        #if self.bus.address.value:
-         #   assert self.bus.data.value== A|b
+        #assert y_fifo=a_fifo|b_fifo
+        #if self.bus.address.value==3:
+         #   temp=y_fifo
+         #   await clk
+         #   assert self.bus.data.value== temp
+        #if dut.write_address.value==4:
+         #   assert a==full
+         #   assert y_fifo==full
+        #if dut.write_address.value==5:
+         #   assert b==full
+         #   assert y_fifo==full
+
+
         if case==1:
             assert self.bus.data.value==1,f"incorrect case 1"
         if case==2 | case==3:
